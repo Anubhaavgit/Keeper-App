6e7d04a038d418016d33cd507f41eea7523b3f4b
Anubhav Mishra <mishra10anubhav@gmail.com>
2021-07-01 19:13:52 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//jshint esversion:8
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
